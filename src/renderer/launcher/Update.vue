5adad75c64ff35fd80e58fdb2575545228492dff
simonzg <zhanghan.simon@gmail.com>
2020-05-18 22:22:44 +0800

    release 1.1.7 add support to version update

--- src/renderer/launcher/Update.vue
+++ src/renderer/launcher/Update.vue
@@ -5,7 +5,7 @@
       <v-card-text>
         <span>About Meter</span>
         <br />
-        <span class="grey--text text--darken-1">Wallet {{getVersion()}} / Flex {{connexV}}</span>
+        <span class="grey--text text--darken-1">Wallet {{getVersion()}} / Flex {{flexVersion}}</span>
       </v-card-text>
       <v-divider />
       <v-list>
@@ -14,14 +14,15 @@
             <v-list-tile-title class="grey--text text--darken-3">Check Update</v-list-tile-title>
           </v-list-tile-content>
           <v-list-tile-action>
+            {{autoUpdateStatusText}}
             <v-btn
-              color="primary"
-              small
-              class="font-weight-regular"
-              @click.stop="checkUpdate"
+              style="text-transform:none"
               flat
-              ripple
-            >Check</v-btn>
+              small
+              color="primary"
+              :disabled="!autoUpdateAction"
+              @click="autoUpdateAction"
+            >{{autoUpdateActionName}}</v-btn>
           </v-list-tile-action>
         </v-list-tile>
       </v-list>
@@ -33,14 +34,67 @@ import { Vue, Component, Watch, Emit } from "vue-property-decorator";
 import { State } from "vuex-class";
 import { remote } from "electron";
 
+const updateChecker = remote.app.EXTENSION.updateChecker;
+
 @Component
 export default class AutoUpdate extends Vue {
   name = "auto_update";
-  connexV = flex.version;
+  flexVersion = flex.version;
+  updater = {
+    status: updateChecker.status,
+    newVersion: updateChecker.newVersion,
+    error: updateChecker.error
+  };
 
   getVersion() {
     return remote.app.getVersion();
   }
+
+  get autoUpdateStatusText() {
+    if (this.updater.status === "downloaded" && this.updater.newVersion) {
+      return `New version ${this.updater.newVersion.version} available!`;
+    }
+    if (this.updater.error) {
+      return "Error occurred";
+    }
+    if (this.updater.status === "idle") {
+      return "Already up to date";
+    }
+  }
+
+  get autoUpdateActionName() {
+    switch (this.updater.status) {
+      case "idle":
+        return "Check";
+      case "checking":
+        return "Checking…";
+      case "downloading":
+        return "Downloading…";
+      case "downloaded":
+        return "Quit and Install";
+    }
+  }
+
+  get autoUpdateAction() {
+    switch (this.updater.status) {
+      case "idle":
+        return () => {
+          updateChecker.check();
+          this.updater = {
+            status: updateChecker.status,
+            newVersion: updateChecker.newVersion,
+            error: updateChecker.error
+          };
+        };
+      case "checking":
+        return undefined;
+      case "downloading":
+        return undefined;
+      case "downloaded":
+        return () => updateChecker.quitAndInstall();
+    }
+  }
+
   @State
   preferencesRevision!: number;
 }
