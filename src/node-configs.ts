6f6af6e902eaeb4d1c8506d2ea3bf2f442242a6b
simonzg <zhanghan.simon@gmail.com>
2021-01-06 17:52:09 +0800

    update genesis config for new warringstakes

--- src/node-configs.ts
+++ src/node-configs.ts
@@ -48,7 +48,7 @@ export const presets: NodeConfig[] = [
       txsRoot:
         "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
       stateRoot:
-        "0x978ff5d68fb51e5fa1de610a8fa4f5d410f72f199f536ee06098ff35e404dcb0",
+        "0x1edc14ab2aa8f0e63ddb4213354bc85b6f51448d146a2f6a648e433eb1733ed5",
       receiptsRoot:
         "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
       signer: "0x0000000000000000000000000000000000000000",
@@ -101,12 +101,8 @@ export function nameOfNetwork(genesisId: string) {
   switch (genesisId) {
    case "0x00000000733c970e6a7d68c7db54e3705eee865a97a07bf7e695c63b238f5e52":
       return "mainnet";
-
-   case "0x00000000ed77a5a4cc2cb585ed7fba4200b89751142cd6fe124aecc3d3350e58":
-      return "testnet";
-  
     case "0x000000003383aa3278b83f8c66d7ec335d5b1409fc832b8dd627c55dd8213665":
-      return "devnet";
+      return "testnet";
     case "0x00000000dc655159094b6c6fa70e475d295fe78599da280ae55b7e54caef8306":
       return "devnet";
     default:
